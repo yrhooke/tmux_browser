1903821a711f81f86a4657406df4b084e6a3cd69
yrhooke <yotamvonreshef@gmail.com>
2019-10-26 23:11:15 +0300

    elm interactive hello world working

--- main.js
+++ main.js
@@ -1,39 +1,49 @@
-'use strict'
-const electron = require('electron')
-
-const app = electron.app // this is our app
-const BrowserWindow = electron.BrowserWindow // This is a Module that creates windows  
-
+const { app, BrowserWindow } = require('electron')
 let mainWindow // saves a global reference to mainWindow so it doesn't get garbage collected
 
-app.on('ready', createWindow) // called when electron has initialized
+console.log(app);
+function initialize() {
 
-// This will create our app window, no surprise there
-function createWindow () {
-    mainWindow = new BrowserWindow({
-        width: 1024, 
-        height: 768
+    // This will create our app window, no surprise there
+    function createWindow() {
+        mainWindow = new BrowserWindow({
+            width: 1024,
+            height: 768,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        })
+
+        // display the index.html file
+        mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+        // open dev tools by default so we can see any console errors
+        mainWindow.webContents.openDevTools()
+
+        mainWindow.on('closed', function () {
+            mainWindow = null
+        })
+    }
+
+    app.on('ready', () => {
+        createWindow()
     })
 
-    // display the index.html file
-    mainWindow.loadURL(`file://${ __dirname }/index.html`)
+    /* Mac Specific things */
 
-    // open dev tools by default so we can see any console errors
-    mainWindow.webContents.openDevTools()
-
-    mainWindow.on('closed', function () {
-        mainWindow = null
+    // when you close all the windows on a non-mac OS it quits the app
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
     })
+
+    app.on('activate', () => {
+        if (mainWindow === null) {
+            createWindow()
+        }
+    })
+
 }
 
-/* Mac Specific things */
-
-// when you close all the windows on a non-mac OS it quits the app
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') { app.quit() }
-})
-
-// if there is no mainWindow it creates one (like when you click the dock icon)
-app.on('activate', () => {
-    if (mainWindow === null) { createWindow() }
-})
+initialize()
